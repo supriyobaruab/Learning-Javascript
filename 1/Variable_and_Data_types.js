e5f4cde06332d0779d3455fb56495d7eff2c04dc
priyosu <suxpriyo@gmail.com>
2024-08-30 11:15:08 +0600

    added practice file

--- 1/Variable_and_Data_types.js
+++ 1/Variable_and_Data_types.js
@@ -1,4 +1,16 @@
-Myname = "Supriyo";
-age    = 24;
-
+let Myname = "Supriyo";
+const age    = 24;
 console.log("My name is " + Myname + " and I am " + age + " years old.");
+let herage = age-1;
+console.log("Her age is " + herage + " years old.");
+
+//Data type in JavaScript
+//String("string"),number,boolean(True,False),Undefined,Symbol,BigInt;
+
+const Student = {
+    name  : "Supriyo",
+    age   : 24,
+    grade : 85,
+    ispass : true
+};
+console.log(Student);
