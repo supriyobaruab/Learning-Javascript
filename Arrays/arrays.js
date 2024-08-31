837b7575da570501539afb1c93b1462e36d73e96
priyosu <suxpriyo@gmail.com>
2024-09-01 05:04:56 +0600

    added array files

---
+++ Arrays/arrays.js
@@ -0,0 +1,14 @@
+//Arrays
+let students = ["Shaown","Aongkon","Eemong"];/*
+console.log(students[1]);
+students[1]="Goru"; // Mutable array
+console.log(students[1]);
+*/
+//Looping over an array
+
+for (let i=0; i<students.length; i++){
+    console.log(students[i]);
+}
+for (let j of students){
+    console.log(j.toUpperCase());
+}
---
+++ Arrays/practice.js
@@ -0,0 +1,16 @@
+/*let marks =[85,97,44,37,76,60]
+let sum =0;
+for(let i=0; i<marks.length; i++){ 
+    sum = sum + marks[i];
+}
+console.log(sum);
+let avrg = sum/marks.length;
+console.log(avrg);//66.5
+*/
+let items = [250,645,300,900,50];
+
+for(let val in items){
+    offer = val/10%;
+    console.log(offer)
+
+}
