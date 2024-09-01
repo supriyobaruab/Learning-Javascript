7b76349dbb2b3ddd743ba02a6494405a3792eff4
priyosu <suxpriyo@gmail.com>
2024-09-01 12:08:10 +0600

    Added some changes

--- Arrays/arrays.js
+++ Arrays/arrays.js
@@ -6,16 +6,28 @@ console.log(students[1]);
 */
 //Looping over an array
 
-for (let i=0; i<students.length; i++){
+/*for (let i=0; i<students.length; i++){
     console.log(students[i]);
 }
 for (let j of students){
     console.log(j.toUpperCase());
-}
+}*/
 
 //Array methods 
 /* 
-*Push():
-*Pop():
-*toString():
+*push(): //add to end
+*pop(): // delete from end & return
+*toString(): // converts array to string
+*concat(): // joins arrays and return new array
+*unshift();// same as Push but from starting
+*shift(); // same as pop but from the starting
+slice(); // cut a slice from array, and return a new array
+splice(); // same but it changes the original array // take e values(start, removed, replace);
+
+//practice
+tudents.push("Debongshi");
+console.log(students);
+let deletedvalue =students.pop();
+console.log(students);
+console.log("removed student is",deletedvalue);
 */
--- Arrays/practice.js
+++ Arrays/practice.js
@@ -7,7 +7,7 @@ console.log(sum);
 let avrg = sum/marks.length;
 console.log(avrg);//66.5
 */
-let items = [250,645,300,900,50];
+/*let items = [250,645,300,900,50];
 
 for(let i=0; i<items.length;i++){
     console.log("Real Price",items[i]);
@@ -15,4 +15,13 @@ for(let i=0; i<items.length;i++){
     items[i] -= offers;
     console.log("Price decreased",offers);
     console.log("Offer price",items[i]);
-}
+}*/
+
+let companies = ["Bloomberg","Microsoft","Uber","Google","IBM","Netflix"]
+console.log(companies);
+companies.shift();
+console.log(companies);
+companies[1]="Ola";
+console.log(companies);
+companies.push("Amazon");
+console.log(companies);
