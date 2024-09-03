b88b2d89e6ee1c4ad3b1afd93bd6038435cd4191
priyosu <suxpriyo@gmail.com>
2024-09-03 13:47:53 +0600

    added another practice, lecture 5 ,43:54

--- function_and_methods/practice.js
+++ function_and_methods/practice.js
@@ -1,4 +1,4 @@
-function countVowels(str){
+/*function countVowels(str){
     let count = 0;
     for(let char of str){
     if (char ==="a" || char ==="e" ||char ==="i" || char ==="o" ||char ==="u"
@@ -7,4 +7,11 @@ function countVowels(str){
     }
     }
     return count;
-}
+}*/
+let arr = [1,2,3,4,5];
+
+arr.forEach((val) => {
+    console.log("before squaring the value",val);
+    val = val*val;
+    console.log("after  squaring the value",val);
+})
