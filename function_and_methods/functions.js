636e6a00af4cd3628e35533fea124a69f97f4153
priyosu <suxpriyo@gmail.com>
2024-09-03 14:03:12 +0600

    function_and_methods/functions: added 2 new methods

--- function_and_methods/functions.js
+++ function_and_methods/functions.js
@@ -12,14 +12,21 @@ const arrowSum = (a,b) =>{
 let goro = [1,2,3,4,5];
 
 goro.forEach(function printval(val){
-    console.log(val);
+    //console.log(val);
     
 })
 goro.forEach((val ,idx,goro)=> {
-    console.log(val,idx,goro);
+    //console.log(val,idx,goro);
 })
 //array methods 
 /*
 *map methods // same as forEach just create a new array;
 */
+//filter methods
+let newArr = [1,2,3,4,5,6];
+
+new = newArr.filter((val) => {
+    return val % 2  ===0;
+});
+console.log(new);
 
