4238e57d657a3dd69210d29767a408ef02be43b3
priyosu <suxpriyo@gmail.com>
2024-09-04 07:19:35 +0600

    added append methods

--- Dom2/dom2.js
+++ Dom2/dom2.js
@@ -1,4 +1,20 @@
-let div = document.querySelector("box");
+/*let div = document.querySelector("#box");
 console.log(div);
 let id = div.getAttribute("id");
 console.log(id);
+div.setAttribute("id","newid");
+console.log(id);
+
+let div = document.querySelector("#box");
+
+div.style.backgroundColor = prompt("print the colour");
+div.style.borderColor = prompt("print the border");
+*/
+let newdiv =document.createElement("div");
+let newbtn =document.createElement("button");
+console.dir(newbtn);
+newbtn.setAttribute("id","btn");
+let div = document.querySelector("div");
+div.append(newdiv);
+newdiv.append(newbtn);
+newbtn.innerText = "Click me!";
