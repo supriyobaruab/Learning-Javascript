8321811af94836e9f504e1a6dc2e64ddbebbfa84
priyosu <suxpriyo@gmail.com>
2024-09-04 09:52:36 +0600

    Changed the name

---
+++ dom/dom1.js
@@ -0,0 +1,15 @@
+// Tom manipulation 
+/*
+let body = document.getElementsByTagName("body")
+console.dir(body);
+//Query selector
+let firstel = document.querySelector("p");
+console.dir(firstel);
+//Modify Properties
+console.dir(document.body);*/
+/*
+let div = document.getElementById("two");
+console.dir(div);*/
+let div = document.getElementsByClassName("divdiv");
+console.dir(div);
+
