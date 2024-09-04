48ebad762f6a456ca6e2a475dc67a5d8d16e1fe9
priyosu <suxpriyo@gmail.com>
2024-09-04 15:12:31 +0600

    bug fixed

--- function_and_methods/practice.js
+++ function_and_methods/practice.js
@@ -8,30 +8,10 @@
     }
     return count;
 }*/
-/*let arr = [1,2,3,4,5];
+let arr = [1,2,3,4,5];
 
 arr.forEach((val) => {
     console.log("before squaring the value",val);
     val = val*val;
     console.log("after  squaring the value",val);
 })
-*/
-let result = [87,89,54,35,90,91,65,98];
-let output = result.filter((val) =>{
-    if (val >=90){
-        return val
-    }
-})
-console.log(output);
-// take input from a user and calculate the sum of the array using the reduce methods
-let n = prompt("enter a number");
-
-let arr =[];
-
-for(let i=1;i<=n;i++){
-    arr[i-1] = i;
-}
-let out = arr.reduce((prev,cur) =>{
-    return prev+cur;
-})
-console.log(out);
