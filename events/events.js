f86ac71011098b9ba6b7d475e0cba669a0f9c606
priyosu <suxpriyo@gmail.com>
2024-09-05 16:35:07 +0600

    added js on button clicked, double clicked and onmouse over

---
+++ events/events.js
@@ -0,0 +1,13 @@
+let btn1 = document.querySelector("#btn1");
+btn1.onclick = () =>{
+    console.log("Clicked");
+
+}
+let btn2 = document.querySelector("#btn2");
+btn2.ondblclick =() =>{
+    console.log("clicked twice");
+}
+let box = document.querySelector(".box")
+box.onmouseover = () =>{
+    console.log("You are on the div file");
+}
