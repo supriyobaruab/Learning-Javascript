f9f967cf191d2f595fbdf232193bf1cafbdbfa10
priyosu <suxpriyo@gmail.com>
2024-09-09 16:24:45 +0600

    almost done

--- rockpaperscissors/app.js
+++ rockpaperscissors/app.js
@@ -1,5 +1,9 @@
 let image     = document.querySelectorAll(".image");
-
+let userScore = document.querySelector("#user-score");
+let compScore = document.querySelector("#comp-score");
+let activities= document.querySelector(".msg");
+let userCount = 0;
+let compCount = 0;
 const genarate =() =>{
     let choices = ["rock","paper","scissors"];
     let compchoice = Math.floor(Math.random() * 3);
@@ -23,15 +27,41 @@ const playGame = (user) =>{
         }
         else if(compchoice == "paper" && user=="rock"){
             console.log("Computer Won");
+            compCount++;
+            compScore.innerText = compCount;
+            console.log(compCount);
+            console.log(userCount);
+            activities.innterText="You lose, paper covered rock";
+
         }
         else if(compchoice =="rock" && user == "scissors"){
             console.log("Computer Won");
+            console.log("Computer Won");
+            compCount++;
+            compScore.innerText = compCount;
+            console.log(compCount);
+            console.log(userCount);
+            activities.innterText="You lose, rock breaks scissors";
+
         }
         else if(compchoice =="scissors" && user == "paper"){
             console.log("Computer Won");
+            compCount++;
+            compScore.innerText = compCount;
+            console.log(compCount);
+            console.log(userCount);
+            activities.innterText="You lose, scissors cut paper";
+
         }
         else{
             console.log("User win");
+            userCount++;
+            userScore.innerText = userCount;
+            console.log(compCount);
+            console.log(userCount);
+            activities.innterText="You win, you beat computer";
         }
         })
     });
+
+    activities.innnerText = "goru";
